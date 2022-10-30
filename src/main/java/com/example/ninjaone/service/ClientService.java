cd39bebe2e3b81a8086fd6919a9a7c2c0afc20bc
Guilherme Silva <guilherme.silva@ab-inbev.com>
2022-10-29 23:56:30 -0300

    Refactor

--- src/main/java/com/example/ninjaone/service/ClientService.java
+++ src/main/java/com/example/ninjaone/service/ClientService.java
@@ -13,6 +13,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -38,21 +40,21 @@ public class ClientService
             .findById(id)
             .orElseThrow(() -> new ValidOperationException(String.format(NOT_FOUND, id)));
     final var clientResponse = clientMapper.toResponse(client);
-    clientResponse.setServices(
-        serviceIds.stream()
-            .map(
-                i -> {
-                  final var service = new ServiceRequest();
-                  service.setId(i);
-                  return service;
-                })
-            .toList());
+    clientResponse.setServices(serviceIds.stream().map(getLongServiceRequestFunction()).toList());
     final var c =
         clientMapper.toResponse(
             clientRepository.save(clientMapper.tofromResponseToEntity(clientResponse)));
     calcCost(c);
     CompletableFuture.runAsync(() -> clientRepository.save(clientMapper.tofromResponseToEntity(c)));
     return c;
+  }
+
+  private Function<Long, ServiceRequest> getLongServiceRequestFunction() {
+    return i -> {
+      final var service = new ServiceRequest();
+      service.setId(i);
+      return service;
+    };
   }
 
   public ClientResponse setDevices(Long id, Set<Long> deviceIds) {
@@ -61,15 +63,7 @@ public class ClientService
             .findById(id)
             .orElseThrow(() -> new ValidOperationException(String.format(NOT_FOUND, id)));
     final var clientResponse = clientMapper.toResponse(client);
-    clientResponse.setDevices(
-        deviceIds.stream()
-            .map(
-                i -> {
-                  final var device = new DeviceRequest();
-                  device.setId(i);
-                  return device;
-                })
-            .toList());
+    clientResponse.setDevices(deviceIds.stream().map(getLongDeviceRequestFunction()).toList());
     final var c =
         clientMapper.toResponse(
             clientRepository.save(clientMapper.tofromResponseToEntity(clientResponse)));
@@ -78,38 +72,53 @@ public class ClientService
     return c;
   }
 
+  private Function<Long, DeviceRequest> getLongDeviceRequestFunction() {
+    return i -> {
+      final var device = new DeviceRequest();
+      device.setId(i);
+      return device;
+    };
+  }
+
   private void calcCost(ClientResponse clientResponse) {
     Map<String, BigDecimal> countByServiceType = new HashMap<>();
-    clientResponse
-        .getServices()
-        .forEach(
-            service -> {
-              countByServiceType.put(
-                  service.getType(),
-                  countByServiceType
-                      .getOrDefault(service.getType(), BigDecimal.ZERO)
-                      .add(service.getCost()));
-            });
+    clientResponse.getServices().forEach(getServiceRequestConsumer(countByServiceType));
     Map<String, BigDecimal> quantityByDeviceType = new HashMap<>();
-    clientResponse
-        .getDevices()
-        .forEach(
-            device -> {
-              quantityByDeviceType.put(
-                  device.getType(),
-                  quantityByDeviceType
-                      .getOrDefault(device.getType(), BigDecimal.ZERO)
-                      .add(BigDecimal.ONE));
-            });
+    clientResponse.getDevices().forEach(getDeviceRequestConsumer(quantityByDeviceType));
     final var totalCost =
         quantityByDeviceType.keySet().stream()
-            .map(
-                type ->
-                    countByServiceType
-                        .getOrDefault(type, BigDecimal.ZERO)
-                        .multiply(quantityByDeviceType.getOrDefault(type, BigDecimal.ZERO)))
+            .map(getStringBigDecimalFunction(countByServiceType, quantityByDeviceType))
             .reduce(BigDecimal::add)
             .orElse(BigDecimal.ZERO);
     clientResponse.setCost(totalCost);
+  }
+
+  private Function<String, BigDecimal> getStringBigDecimalFunction(
+      final Map<String, BigDecimal> countByServiceType,
+      final Map<String, BigDecimal> quantityByDeviceType) {
+    return type ->
+        countByServiceType
+            .getOrDefault(type, BigDecimal.ZERO)
+            .multiply(quantityByDeviceType.getOrDefault(type, BigDecimal.ZERO));
+  }
+
+  private Consumer<DeviceRequest> getDeviceRequestConsumer(
+      final Map<String, BigDecimal> quantityByDeviceType) {
+    return device -> {
+      quantityByDeviceType.put(
+          device.getType(),
+          quantityByDeviceType.getOrDefault(device.getType(), BigDecimal.ZERO).add(BigDecimal.ONE));
+    };
+  }
+
+  private Consumer<ServiceRequest> getServiceRequestConsumer(
+      final Map<String, BigDecimal> countByServiceType) {
+    return service -> {
+      countByServiceType.put(
+          service.getType(),
+          countByServiceType
+              .getOrDefault(service.getType(), BigDecimal.ZERO)
+              .add(service.getCost()));
+    };
   }
 }
